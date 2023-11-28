e511edd37a75181cde085d8f53bae08c903edab8
osiris21 <osiris.orduram@gmail.com>
2023-11-28 16:02:52 -0600

    prueba

--- src/main/java/com/mycompany/lecturaguia/Lecturaguia.java
+++ src/main/java/com/mycompany/lecturaguia/Lecturaguia.java
@@ -1,16 +1,16 @@
-/*
+/*hjghfhgfshgfxhgfhxfdgfdxgdsxfsgfxdhgdhgddfdgfdfdhxyfjfhjxjyx
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
 package com.mycompany.lecturaguia;
 
 /**
- *
+ *gdnhztxngfsnytyfnhtcrjyhngnhtrnhcfhtr
  * @author Ventas Osiris
  */
 public class Lecturaguia {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola gabo!");
     }
 }
